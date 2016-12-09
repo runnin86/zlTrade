b41a76e2597b2ef34bb4c6fe515f5ba681ac4f3e
runnin86 <dda1314@163.com>
2016-12-09 17:23:37 +0800

    2016年12月09日17:23:34

--- src/views/page/productCont.vue
+++ src/views/page/productCont.vue
@@ -1,4 +1,5 @@
 <template>
+  <div class="content" transition="bounce">
   <!--顶部-->
   <div class="com-header">
     <div class="return"><a @click="this.$root.back()"><i class="iconfont">&#xe624;</i></a></div>
@@ -7,8 +8,8 @@
   <div class="header-h"></div>
   <!--顶部:over-->
   <!--图片展示-->
-  <div class="banner">
-    <div class="main_visual">
+  <div class="used-banner">
+    <div class="main_visual" style="height: 100%;">
       <div class="flicking_con">
         <span>
           <a href="#">1</a>
@@ -19,10 +20,10 @@
       </div>
       <div class="main_image">
         <ul>
-          <li><span><a href="#"><img src="/img/pic-nr1.png" style="width:16rem;" alt="" /></a></span></li>
-          <li><span><a href="#"><img src="/img/pic-nr2.png" style="width:16rem;" alt="" /></a></span></li>
-          <li><span><a href="#"><img src="/img/pic-nr1.png" style="width:16rem;" alt="" /></a></span></li>
-          <li><span><a href="#"><img src="/img/pic-nr2.png" style="width:16rem;" alt="" /></a></span></li>
+          <li><span><a href="#"><img src="/img/pic-car1.png"></a></span></li>
+          <li><span><a href="#"><img src="/img/pic-car2.png" style="width:16rem; height: 10.7rem;" alt="" /></a></span></li>
+          <li><span><a href="#"><img src="/img/pic-car1.png" style="width:16rem; height: 10.7rem;" alt="" /></a></span></li>
+          <li><span><a href="#"><img src="/img/pic-car2.png" style="width:16rem; height: 10.7rem;" alt="" /></a></span></li>
         </ul>
         <a href="javascript:;" id="btn_prev"></a>
         <a href="javascript:;" id="btn_next"></a>
@@ -36,30 +37,6 @@
         <h3>迪加伦 汽车钥匙扣 金属 创意 挂件装饰品 bv编织 卡通 车用钥匙链 </h3>
         <p>￥230.00</p>
       </div>
-      <div class="cont-title1-right">
-        <a href="#" class="active"><i class="iconfont">&#xe605;</i><p>收藏</p></a>
-      </div>
-    </div>
-    <div class="cont-title2">
-      <span class="span1">库存：2件</span><span class="span2">已售：2件</span><span class="span3">福建 福州</span>
-    </div>
-  </div>
-  <div class="cont-sel">
-    <a @click="showBuy=true">
-      <strong>已选： 黑色  46码  X1 </strong><i class="icon">&#xe624;</i>
-    </a>
-  </div>
-  <div class="cont-comment">
-    <div class="cont-comment-top"><strong>商品评论</strong></div>
-    <div class="cont-comment-bottom">
-      <ul>
-        <li style="border:none;">
-          <h3><a href="#"><img src="/img/pic-tx.png">听说昵称越长越牛逼</a></h3>
-          <div class="pl-cont">宝贝很好！物超所值！值得购买！非常满意！物流飞快！</div>
-          <div class="pl-time">2016.01-11</div>
-          <div class="pl-more"><a href="goods-comment.html">查看全部评价</a></div>
-        </li>
-      </ul>
     </div>
   </div>
   <div id="tabBox1" class="tabBox">
@@ -67,7 +44,6 @@
       <ul>
         <li><a href="javascript:void(0)">商品介绍</a></li>
         <li><a href="javascript:void(0)">规格参数</a></li>
-        <li><a href="javascript:void(0)">售后保障</a></li>
       </ul>
     </div>
     <div class="bd" id="tabBox1-bd">
@@ -87,24 +63,13 @@
         </div>
       </div>
       <!--规格参数:over-->
-      <!--售后保障-->
-      <div class="con">
-        <div class="cont-container">
-          <p>售后保障后台添加修改售后保障后台添加修改售后保障后台添加修改售后保障后台添加修改售后保障后台添加修改</p>
-          <p>售后保障后台添加修改售后保障后台添加修改售后保障后台添加修改售后保障后台添加修改售后保障后台添加修改</p>
-          <p>售后保障后台添加修改售后保障后台添加修改售后保障后台添加修改售后保障后台添加修改售后保障后台添加修改</p>
-          <p>售后保障后台添加修改售后保障后台添加修改售后保障后台添加修改售后保障后台添加修改售后保障后台添加修改</p>
-          <p>售后保障后台添加修改售后保障后台添加修改售后保障后台添加修改售后保障后台添加修改售后保障后台添加修改</p>
-        </div>
-      </div>
-      <!--售后保障:over-->
     </div>
   </div>
   <!--悬浮-->
   <div class="cont-tool-h"></div>
-  <div class="cont-tool">
-    <input type="button" class="btn-jrgwc" value="加入购物车" @click="showBuy=true">
-    <input type="button" class="btn-ljgm" value="立即购买" @click="showBuy=true">
+  <div class="used-tool">
+    <input type="button" class="used-btn" style="background-color:#ffb744;"
+      value="立即购买" @click="showBuy=true"/>
   </div>
   <!--悬浮:over-->
   <!--购买信息弹窗-->
@@ -117,10 +82,6 @@
       </div>
       <div class="gm-close"><i class="icon" @click="showBuy=false">&#xe62f;</i></div>
     </div>
-    <div class="buy-info2">
-      <p><span class="span1">颜色：</span><span class="span2"><label class="active">蓝色</label><label>红色</label><label>青色</label><label>白色</label><label>红色</label><label>青色</label><label>白色</label></span></p>
-      <p><span class="span1">尺寸：</span><span class="span2"><label class="active">40码</label><label>41码</label><label>42码</label><label>43码</label><label>44码</label><label>45码</label></span></p>
-    </div>
     <div class="buy-info3">
       <strong>购买数量</strong>
       <div class="num">
@@ -132,13 +93,14 @@
   </div>
   <div class="black" id="buy-black" @click="showBuy=false" :style="{'display': (showBuy?'block':'none')}"></div>
   <!--购买信息弹窗-->
+  </div>
 </template>
 
 <script>
 require('../../assets/js/jquery.min')
 require('../../assets/js/jquery.event.drag-1.5.min')
 require('../../assets/js/jquery.touchSlider')
-require('../../assets/js/banner')
+require('../../assets/js/used-banner')
 let {TouchSlide} = require('../../assets/js/TouchSlide.1.1')
 
 export default {
